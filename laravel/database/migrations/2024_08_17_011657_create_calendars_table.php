a4f04e9c30e26a029d7cc3279c8ae649517c04bf
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-08-28 23:42:01 +0900

    add テーブル追加

---
+++ laravel/database/migrations/2024_08_17_011657_create_calendars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calendars', function (Blueprint $table) {
+            $table->id();
+            $table->string('team_name');
+            $table->string('memo')->nullable();
+            $table->string('url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calendars');
+    }
+};
