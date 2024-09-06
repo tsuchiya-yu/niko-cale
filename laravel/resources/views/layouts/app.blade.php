4428dd40fd325cbf10d61336369680554f773eb7
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-09-07 01:10:31 +0900

    fix ヘッダー見直し

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -2,13 +2,20 @@
 <html lang="ja">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="Content-Language" content="ja">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- TODO: 画像は仮 --}}
+    <meta property="og:image" content="{{ Vite::asset('resources/images/calendar_capture.png') }}">
+    {{-- TODO: 未定 --}}
+    <meta property="og:url" content="">
     
     <!-- タイトル -->
     @hasSection('title')
         <title>@yield('title') | ニコカレ</title>
+        <meta property="og:title" content="@yield('title') | ニコカレ">
     @else
         <title>ニコカレ | 誰でも簡単にニコニコカレンダーが作れるツール</title>
+        <meta property="og:title" content="ニコカレ | 誰でも簡単にニコニコカレンダーが作れるツール">
     @endif
     <!-- robots -->
     @if (View::hasSection('index') && trim(View::getSection('index')) === 'true')
@@ -19,8 +26,10 @@
     <!-- description -->
     @hasSection('description')
         <meta name="description" content="@yield('description')">
+        <meta property="og:description" content="@yield('description')">
     @else
-        <meta name="description" content="ニコカレは、チームのモチベーション管理ツール「ニコニコカレンダー」が誰でも簡単に作成できるツールです。">
+        <meta name="description" content="ニコカレは、チームのモチベーションを簡単に管理できる「ニコニコカレンダー」を作成できるツールです。チームの感情を視覚化して、最高のチームにしましょう。">
+        <meta property="og:description" content="ニコカレは、チームのモチベーションを簡単に管理できる「ニコニコカレンダー」を作成できるツールです。チームの感情を視覚化して、最高のチームにしましょう。">
     @endif
 
     <link rel="preconnect" href="https://fonts.bunny.net">
