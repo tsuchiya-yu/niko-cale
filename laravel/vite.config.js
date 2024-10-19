8d518427f32295a2a399b5b389dc150c7e336399
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-10-19 17:08:56 +0900

    fix viteの定義でcss、jsを再帰的に取得するよう修正

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,11 +1,44 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import fs from "fs";
+import path from "path";
+
+// 指定されたディレクトリ内のすべてのファイルを再帰的に取得
+function getFilesRecursively(directory) {
+    const filesInDirectory = fs.readdirSync(directory);
+    const files = [];
+
+    for (const file of filesInDirectory) {
+        const absolute = path.join(directory, file);
+        if (fs.statSync(absolute).isDirectory()) {
+            files.push(...getFilesRecursively(absolute));
+        } else {
+            files.push(absolute);
+        }
+    }
+    return files;
+}
+
+// resources/css と resources/js ディレクトリ内のすべてのファイルを取得
+const cssFiles = getFilesRecursively(path.resolve(__dirname, 'resources/css'))
+    .filter(file => file.endsWith('.css'));
+const jsFiles = getFilesRecursively(path.resolve(__dirname, 'resources/js'))
+    .filter(file => file.endsWith('.js'));
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/reset.css', 'resources/js/app.js'],
+            input: [
+                ...cssFiles,
+                ...jsFiles,
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        emptyOutDir: true,
+    },
+    base: 'https://nikocale.fly.dev/',
 });
